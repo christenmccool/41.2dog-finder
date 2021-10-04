f178fdb9835507a1f6ab1e8c0ce4ec811aae4bb6
Christen McCool <christenmccool@Christens-MacBook-Pro.local>
2021-10-04 06:31:05 -0700

    Add go back link and redirect when dog not found

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link, Redirect } from "react-router-dom";
 import "./DogDetails.css"
 
 
@@ -7,17 +7,22 @@ const DogDetails = ({dogs}) => {
   const {name} = useParams();
   const dog = dogs.find(ele => ele.name === name);
 
-  return (
-    <div className="DogDetails">
-      <h1>{dog.name}</h1>
-      <h3>{`Age: ${dog.age}`}</h3>
-      <img src={dog.src} alt={dog.name} />
-      <h3>Facts:</h3>
-      <ul>
-        {dog.facts.map(fact => <li>{fact}</li>)}
-      </ul>
-    </div>
-  )
+  if (dog) {
+    return (
+      <div className="DogDetails">
+        <h1>{dog.name}</h1>
+        <h3>{`Age: ${dog.age}`}</h3>
+        <img src={dog.src} alt={dog.name} />
+        <h3>Facts:</h3>
+        <ul>
+          {dog.facts.map(fact => <li>{fact}</li>)}
+        </ul>
+        <Link to="/dogs">Go back</Link>
+      </div>
+    )
+  } else {
+    return <Redirect to="/dogs"/>
+  }
 }
 
 export default DogDetails;
